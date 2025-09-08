adb263140614c036a458a699e110f986726f0b24
Atish1910 <atishkamble398@gmail.com>
2025-09-08 23:09:03 +0530

    03

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import Header from "./components/header";
-import Hero from "./components/hero";
+import Products from "./components/Products";
 import Footer from "./components/footer";
 function App() {
   return (
     <>
       <section>
         <Header></Header>
-        <Hero></Hero>
+        <Products></Products>
         <Footer></Footer>
       </section>
     </>
---
+++ src/components/Products.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <>
+      <div class="container col-lg-8  py-5">
+        <div class="row  align-items-center py-5">
+          <div className="col-lg-3">
+            <div className="card">
+              <img src="" alt="" />
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Products;
